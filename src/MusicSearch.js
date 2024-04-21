0e50be2dbc2f942cf76ea4565bb35e28a37b2825
Eduardo Iwamoto <eduardoiwamoto2020@gmail.com>
2024-04-21 07:57:16 -0300

    validations

--- src/MusicSearch.js
+++ src/MusicSearch.js
@@ -1,3 +1,4 @@
+// MusicSearch.js
 import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import MusicDetailsPopup from './MusicDetailsPopup';
@@ -6,23 +7,34 @@ const MusicSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [selectedResult, setSelectedResult] = useState(null);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    if (searchTerm.trim() === '') return;
+    setSearchResults([]);
+    setError('');
+
+    if (searchTerm.trim() === '') {
+      return;
+    }
 
     const fetchData = async () => {
       try {
         const response = await axios.get(`https://itunes.apple.com/search?term=${searchTerm}&entity=musicTrack&limit=10`);
-        setSearchResults(response.data.results);
+        const results = response.data.results;
+
+        if (results.length === 0) {
+          setError('Nenhum resultado encontrado');
+        } else {
+          setSearchResults(results);
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
+        setError('Nenhum resultado encontrado');
       }
     };
 
     fetchData();
   }, [searchTerm]);
-
-  const memoizedResults = useMemo(() => searchResults, [searchResults]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -34,12 +46,15 @@ const MusicSearch = () => {
 
   return (
     <div>
-      <input type="text" value={searchTerm} onChange={handleSearchChange} />
-      <ul>
-        {memoizedResults.map((result) => (
-          <li key={result.trackId} onClick={() => handleResultClick(result)}>{result.trackName} - {result.artistName}</li>
-        ))}
-      </ul>
+      <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Pesquisar Músicas" />
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {searchResults.length > 0 && (
+        <ul>
+          {searchResults.map((result) => (
+            <li key={result.trackId} onClick={() => handleResultClick(result)}>{result.trackName} - {result.artistName}</li>
+          ))}
+        </ul>
+      )}
       <MusicDetailsPopup isOpen={selectedResult !== null} onClose={() => setSelectedResult(null)} details={selectedResult} />
     </div>
   );
